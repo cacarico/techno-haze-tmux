416869e2557b544b9547874483149f66d9816181
cacarico <caio.quinilato@gmail.com>
2023-11-04 02:42:36 +0100

    Add base

---
+++ cacarico.tmux
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Sources theme
+
+current_dir="$PWD"
+$current_dir/scripts/base.sh
---
+++ scripts/base.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export LC_ALL=en_US.UTF-8
+
+main(
+
+    # Cacarico Color Pallette
+    gray='#7A7276DB'
+    pink='#D100AEDB'
+
+tmux set-option -g status-left "#[fg=${grey}]#{?client_prefix,#[bg=${pink}],} ${icon} #[bg=${grey}]#{?client_prefix,#[fg=${pink}]}"
+
+)
