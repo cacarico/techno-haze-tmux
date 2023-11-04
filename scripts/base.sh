4855cee72a3551c30d662c0810c654ca351ac635
cacarico <caio.quinilato@gmail.com>
2023-11-04 03:35:12 +0100

    Adds heart

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,13 +2,21 @@
 
 export LC_ALL=en_US.UTF-8
 
-main(
+main() {
 
     # Cacarico Color Pallette
     icon="💜"
+    icon_toggled="🩷"
     gray='#7A7276DB'
     pink='#D100AEDB'
 
-tmux set-option -g status-left "#[fg=${grey}]#{?client_prefix,#[bg=${pink}],} ${icon} #[bg=${grey}]#{?client_prefix,#[fg=${pink}]}"
+      # set length
+      tmux set-option -g status-left-length 100
+      tmux set-option -g status-right-length 100
 
-)
+
+      tmux set-option -g status-left " #{?client_prefix,${icon_toggled},${icon}} "
+      tmux set-option -g status-style "bg=default"
+}
+
+main
