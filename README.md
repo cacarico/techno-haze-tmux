39490e30ac22557fbb824eb39b616bfdb5342c73
cacarico <caio.quinilato@gmail.com>
2024-05-11 19:39:41 +0200

    Update techno haze

--- README.md
+++ README.md
@@ -1,3 +1,94 @@
-# Techno Haze Tmux Theme
+# Techno Haze Tmux
 
-A minimalistic theme for tmux focused on productivity in a cluster free layout.
+A minimalistic setup for tmux focused on productivity in a clutter-free layout.
+
+## Overview
+
+The Techno Haze Tmux offers a clean and minimalist interface tailored for enhanced productivity. With a focus on simplicity and efficiency, this theme provides a clutter-free layout to help you stay focused on your tasks.
+
+## Key Features
+
+- **Window Management**: Easily create new sessions and switch between windows.
+- **Pane Navigation**: Effortlessly move and resize panes for optimal organization.
+- **Ready to Use**: It comes packed with keybidings to let you start using tmux in productive way from the day 0
+
+## Installation
+To install the Techno Haze Tmux, you can use [TPM](https://github.com/tmux-plugins/tpm) or clone the repo localy.
+
+### Installing with Tmux Plugin Manager
+
+
+Update your `~/.tmux.conf` file to use the plugin:
+
+```
+set -g @plugin 'cacarico/techno-haze-tmux'
+```
+
+Install plugins using `prefix + I` (NOTE: prefix by default is ctrl + b)
+
+### Installing Manually
+
+```
+git clone https://github.com/cacarico/techno-haze-tmux.git ~/.config/tmux/plugins/techno-haze-tmux
+```
+
+And add the following line to your  `~/.tmux.conf` file:
+
+```
+run-shell "~/.config/tmux/plugins/techno-haze-tmux/techno-haze.tmux"
+```
+
+Install plugins using `prefix + I` (NOTE: prefix by default is ctrl + b)
+
+## Usage
+
+Once installed, you can start using the Techno Haze Tmux immediately. Here are some key bindings to get you started:
+
+- **Alt + a**: Prefix key for tmux commands.
+- **Prefix + r**: Reloads the tmux configuration.
+
+| Key Combination | Functionality                 |
+|-----------------|-------------------------------|
+| Prefix + a      | Zoom the current panel        |
+| Prefix + r      | Reloads the tmux configuration.
+| Prefix + -      | Splits plane horizontally
+| Prefix + \      | Splits plane vertically
+| A-Tab      | Switch to window to the right
+| A-S-Tab    | Switch to window to the left                               |
+| A-C-s      | Swap window x with window y                                |
+| A-C-h      | Swap pane with the pane to the left                        |
+| A-C-k      | Swap pane with the pane above                              |
+| A-C-j      | Swap pane with the pane below                              |
+| A-C-l      | Swap pane with the pane to the right                       |
+| A-h        | Select pane to the left                                    |
+| A-j        | Select pane downward                                       |
+| A-k        | Select pane upward                                         |
+| A-l        | Select pane to the right                                   |
+| A-H        | Resize pane to the left by 4 columns                       |
+| A-J        | Resize pane downward by 2 rows                             |
+| A-K        | Resize pane upward by 2 rows                               |
+| A-L        | Resize pane to the right by 4 columns                      |
+| A-0 to M-9 | Switch to windows 1 to 10, with M-0 switching to window 10 |
+
+For more details on usage and customization options, refer to the theme documentation or the comments within the `techno-haze.tmux` file.
+
+## Credits
+
+This theme is inspired by the need for a distraction-free environment in tmux. Special thanks to the tmux community for their valuable contributions and feedback.
+
+## Contributions
+
+Contributions to the Techno Haze Tmux Theme are welcome! Feel free to submit pull requests, report issues, or suggest improvements via the project's GitHub repository.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE), allowing for both personal and commercial use with attribution. See the LICENSE file for details.
+
+Enjoy your clutter-free tmux experience with the Techno Haze Tmux Theme!
+
+## List of Key Bindings
+
+- Alt + a: Prefix key for tmux commands.
+- Alt + r: Reloads the tmux configuration.
+- Alt + 1-4: Switches to windows 1-4.
+- Alt + Ctrl + h/j/k/l: Moves and resizes panes for optimal layout.
--- scripts/techno-haze.sh
+++ scripts/techno-haze.sh
@@ -6,17 +6,142 @@ export LC_ALL=en_US.UTF-8
 
 #TODO Create own function, maybe a utils.sh makes sense.
 get_tmux_option() {
-  local option=$1
-  local default_value=$2
-  local option_value=$(tmux show-option -gqv "$option")
-  if [ -z "$option_value" ]; then
-    echo $default_value
-  else
-    echo $option_value
-  fi
+    local option=$1
+    local default_value=$2
+    local option_value=$(tmux show-option -gqv "$option")
+    if [ -z "$option_value" ]; then
+        echo $default_value
+    else
+        echo $option_value
+    fi
+}
+
+bind_shell() {
+    key="$1"
+    command="$2"
+
+    tmux bind-key -n "$key" run-shell "$command"
+}
+
+bind_key() {
+    key="$1"
+    command="$2"
+
+    tmux bind-key -n "$key" "$command"
+}
+
+bind_prefix() {
+    key="$1"
+    command="$2"
+
+    tmux bind "$key" "$command"
+}
+
+bind_vim() {
+    key="$1"
+    command="$2"
+    command2="$3"
+
+    is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+        | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
+
+    tmux bind-key -n "$key" if-shell "$is_vim" "$command" "$command2"
+}
+
+setup_keys() {
+
+    # Reload configuration
+    tmux bind r source-file ~/.config/tmux/tmux.conf \; display '~/.tmux.conf reloaded'
+
+    # Bind Ctrl + Alt + s to swap panes
+    tmux bind-key -n C-M-s command-prompt -p "Swap window:","With Window:" "swap-window -s '%1' -t '%2'"
+
+
+    # Integration between Nvim and Tmux to change panes naturally
+    # pane resizing
+    bind_vim 'M-h' 'send-keys M-h' 'select-pane -L'
+    bind_vim 'M-j' 'send-keys M-j' 'select-pane -D'
+    bind_vim 'M-k' 'send-keys M-k' 'select-pane -U'
+    bind_vim 'M-l' 'send-keys M-l' 'select-pane -R'
+    bind_vim 'M-H' 'send-keys M-H' 'resize-pane -L 4'
+    bind_vim 'M-J' 'send-keys M-J' 'resize-pane -D 2'
+    bind_vim 'M-K' 'send-keys M-K' 'resize-pane -U 2'
+    bind_vim 'M-L' 'send-keys M-L' 'resize-pane -R 4'
+    bind_vim 'M-C-h' 'send-keys M-C-h' 'run-shell "tmux select-pane -L \\; swap-pane -d -s #D"'
+    bind_vim 'M-C-k' 'send-keys M-C-k' 'run-shell "tmux select-pane -U \\; swap-pane -d -s #D"'
+    bind_vim 'M-C-j' 'send-keys M-C-j' 'run-shell "tmux select-pane -D \\; swap-pane -d -s #D"'
+    bind_vim 'M-C-l' 'send-keys M-C-l' 'run-shell "tmux select-pane -R \\; swap-pane -d -s #D"'
+
+    # Swap window x with windows y
+    bind-key -n C-M-s command-prompt -p "Swap window:","With Window:" "swap-window -s '%1' -t '%2'"
+
+    # Select window from 1 to 10
+    for i in $(seq 0 9); do
+        tmux bind-key -n "M-$i" select-window -t "$i"
+        if [ "$i" == 0 ]; then
+            bind_key 'M-0' 'select-window -t 10'
+        fi
+    done
+
+    ## create session
+    # bind_prefix 'C-c' new-session
+
+    # window navigation
+
+    # split current window horizontally
+    # bind_prefix '-' 'split-window -v'
+
+    # split current window vertically
+    tmux bind '-' 'split-window -v'
+    tmux bind '\' 'split-window -h'
+    tmux bind -n 'M-Tab' 'next-window' # select previous window
+    tmux bind -n 'M-BTab' 'previous-window'     # select next window
+
+    # Zoom Window
+    tmux unbind z
+    tmux bind 'a' 'resize-pane -Z'
+}
+
+setup_config() {
+    ## -- general -------------------------------------------------------------------
+
+    # Set Prefix to alt
+    tmux set-option -g prefix M-a
+    tmux bind-key M-a send-prefix
+
+    # Enable mouse integration
+    tmux set -g mouse on
+
+    # Enable 255 color terminal
+    tmux set -g default-terminal "screen-256color"
+
+    # Sets history-limit to 5000 lines
+    tmux set -g history-limit 5000
+
+    # set vim mode
+    tmux set-window-option -g mode-keys vi
+
+    ## -- display -------------------------------------------------------------------
+
+    tmux set -g base-index 1           # Start index in 1
+    tmux set -g renumber-windows on    # Renumber windows
+    tmux set -g display-panes-time 801 # Slightly longer pane indicators display time
+    tmux set -g display-time 1000      # Slightly longer status messages display time
+    tmux set -g status-interval 10     # Redraw status every 10 seconds
+    tmux setw -g pane-base-index 1     # Make pane numbering consistent with windows
+    tmux setw -g automatic-rename off  #
+
+
+
+    # Fix image preview (https://yazi-rs.github.io/docs/image-preview#tmux-users)
+    tmux set -g allow-passthrough on
+    tmux set -ga update-environment TERM
+    tmux set -ga update-environment TERM_PROGRAM
 }
 
 main() {
+    setup_config
+    setup_keys
 
     # Set configurations
     icon_inactive=$(get_tmux_option "@technohaze-icon" purple-heart)
@@ -35,7 +160,6 @@ main() {
     light_purple='#ff2bff'
     rose='#33658A'
     red='#E06666'
-
 
     # Set Colors
     window_color="$purple"
